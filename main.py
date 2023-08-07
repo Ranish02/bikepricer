bc6e34d355f97a9a8ee1990ee3a75cb98d832e3d
Ranish02 <usaranish@gmail.comm>
2023-08-08 00:30:27 +0545

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from Stats import show_explore_page
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
 if page == "Predict":
-     show_predict_page()
+    show_predict_page()
 if page == "Explore":
-   show_explore_page()
-
+    show_explore_page()
--- predictpage.py
+++ predictpage.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pickle
 import numpy as np
 
+
 def load_model():
     with open('./stored_steps.pkl', 'rb') as file:
         data = pickle.load(file)
     return data
+
 
 data = load_model()
 
@@ -13,38 +15,34 @@ regressor = data["model"]
 le_brand = data["le_brand"]
 le_owner = data["le_owner"]
 
+
 def show_predict_page():
-    st.title("Used Bike Price Estimator")
+    st.title("Bike Pricer- Used Bike Price Estimator")
     st.write("""### Estimate your used bike prices""")
     st.write(""" We need some information to estimate the bike price""")
+    st.write(
+        """ This app is based on a model trained using dataset from kaggle """)
+    st.write(
+        "Dataset source: [link](https://www.kaggle.com/datasets/saisaathvik/used-bikes-prices-in-india)")
 
     brand = (
-        'TVS',
-        'Royal Enfield',
-        'Triumph',
-        'Yamaha',
-        'Honda',
-        'Hero',
-        'Bajaj',
-        'Suzuki',
-        'Benelli',
-        'KTM',
-        'Mahindra',
-        'Kawasaki',
-        'Ducati',
-        'Hyosung',
-        'Harley-Davidson',
-        'Jawa',
-        'BMW',
-        'Indian',
-        'Rajdoot',
-        'LML',
-        'Yezdi',
-        'MV',
-        'Ideal'
+        'TVS', 'Royal Enfield', 'Triumph', 'Yamaha',
+        'Honda', 'Hero', 'Bajaj', 'Suzuki',
+        'Benelli', 'KTM', 'Mahindra', 'Kawasaki', 'Ducati',
+        'Hyosung', 'Harley-Davidson', 'Jawa',
+        'BMW', 'Indian', 'Rajdoot', 'LML', 'Yezdi', 'MV', 'Ideal'
     )
-    Power= (
-        100, 107, 110, 125, 135, 149, 150, 160, 175, 180, 200, 220, 223, 250, 295, 300, 302, 310, 320, 350, 390, 400, 410, 500, 502, 535, 600, 650, 675, 750, 765, 796, 797, 800, 821, 850, 865, 883, 899, 900, 959, 1000, 1050, 1090, 1100, 1130, 1198, 1200, 1262, 1299, 1300, 1700, 1800
+    Power = (
+        100, 107, 110, 125,
+        135, 149, 150, 160, 175,
+        180, 200, 220, 223, 250, 295,
+        300, 302, 310, 320, 350, 390, 400,
+        410, 500, 502, 535, 600, 650,
+        675, 750, 765, 796, 797,
+        800, 821, 850, 865, 883, 899,
+        900, 959, 1000, 1050, 1090,
+        1100, 1130, 1198, 1200,
+        1262, 1299, 1300, 1700, 1800
     )
     owner = (
         'First Owner',
@@ -55,15 +53,15 @@ def show_predict_page():
 
     Brand = st.selectbox("Brand", brand)
     Power = st.selectbox("Power (cc)", Power)
-    kms_driven = st.number_input("Bike Driven (in Kilometer)", 0, None, 1000, None)
+    kms_driven = st.number_input(
+        "Bike Driven (in Kilometer)", 0, None, 1000, None)
     Owner = st.selectbox("Owner", owner)
-    age = st.number_input("Bike age (years)",0,50,1)
+    age = st.number_input("Bike age (years)", 0, 50, 1)
     ok = st.button("Estimate")
     if ok:
-        X = np.array([[kms_driven,Owner,age,Power,Brand]])
+        X = np.array([[kms_driven, Owner, age, Power, Brand]])
         X[:, 4] = le_brand.transform(X[:, 4])
         X[:, 1] = le_owner.transform(X[:, 1])
         X = X.astype(float)
         salary = regressor.predict(X)
         st.subheader(f"The estimated salary is ${salary[0]:.2f}")
-
